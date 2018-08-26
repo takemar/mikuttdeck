4ce70f63d3300897556423fcfa3b8c16eaf72997
takemaro <info@takemaro.com>
2018-08-26 22:42:06 +0900

    RTを正しく取得できるようにした
    dataset.tweetIdではなくdataset.keyを見る

--- home_timeline.js
+++ home_timeline.js
@@ -1,9 +1,9 @@
 "use strict";
-var arr = [], element;
+var ids = [], element;
 for (element of arguments[0].childNodes) {
-    if (element.dataset.tweetId == arguments[1]) {
+    if (element.dataset.key == arguments[1]) {
         break;
     }
-    arr.push(element.dataset.tweetId)
+    ids.push(element.dataset.key)
 }
-return arr;
+return ids;
